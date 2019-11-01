f055b8da80f5798311eb13fe65800212757a229c
Peng Li <peng.li@compass.com>
2019-11-01 23:15:46 +0800

    node_modules

--- hello.ts
+++ hello.ts
@@ -1,7 +1,7 @@
 const httpProxy = require('http-proxy');
 
 httpProxy.createProxyServer({
-  target: 'http://localhost:43762'
+  target: 'http://localhost:43762/node_modules'
 }).listen(28378);
 
 console.log('visit http://localhost:28378');
